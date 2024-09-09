3acd196dc4b6e4e3d42594a224e413d43a6ecae4
Minhyuk Kim <labcs@naver.com>
2024-09-09 14:33:52 +0900

    add files

---
+++ coding-test/stack.js
@@ -0,0 +1,36 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack { // 맨 앞에서 삽입, 삭제가 일어나게 하므로 상수 시간 복잡도를 가지게 함
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    pop() {
+        if (!this.first) return null;
+        let temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
+}
