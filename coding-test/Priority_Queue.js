6a21453767b167c0f8d98e47b4e771fa8cbbfdf2
Minhyuk Kim <labcs@naver.com>
2024-09-12 15:01:16 +0900

    add file & edit README

--- coding-test/Priority_Queue.js
+++ coding-test/Priority_Queue.js
@@ -1,9 +1,10 @@
-class MaxBinaryHeap {
+class PriorityQueue {
     constructor() {
         this.values = [];
     }
-    insert(element) {
-        this.values.push(element);
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
         this.bubbleUp();
     }
     bubbleUp() {
@@ -12,20 +13,20 @@ class MaxBinaryHeap {
         while (idx > 0) {
             let parentIdx = Math.floor((idx - 1) / 2);
             let parent = this.values[parentIdx];
-            if (element <= parent) break;
+            if (element.priority >= parent.priority) break;
             this.values[parentIdx] = element;
             this.values[idx] = parent;
             idx = parentIdx;
         }
     }
-    extractMax() {
-        const max = this.values[0];
+    dequeue() {
+        const min = this.values[0];
         const end = this.values.pop();
         if (this.values.length > 0) {
             this.values[0] = end;
             this.sinkDown();
         }
-        return max; //삭제하려는 기존 루트 노드 값 반환
+        return min; //삭제하려는 기존 루트 노드 값 반환
     }
     sinkDown() {
         let idx = 0;
@@ -39,15 +40,15 @@ class MaxBinaryHeap {
 
             if (leftChildIdx < length) {
                 leftChild = this.values[leftChildIdx];
-                if (leftChild > element) {
+                if (leftChild.priority < element.priority) {
                     swap = leftChildIdx;
                 }
             }
             if (rightChildIdx < length) {
                 rightChild = this.values[rightChildIdx];
                 if (
-                    (swap === null && rightChild > element) ||
-                    (swap !== null && rightChild > leftChild)
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
                 ) {
                     swap = rightChildIdx;
                 }
@@ -60,4 +61,13 @@ class MaxBinaryHeap {
         }
     }
 }
+
+class Node {
+    constructor(val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+
 
