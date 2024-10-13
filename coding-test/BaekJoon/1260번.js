0e36b51236120aa0fea391271a31a199dc1d5205
Minhyuk Kim <labcs@naver.com>
2024-10-13 16:02:53 +0900

    add file & edit README

--- coding-test/BaekJoon/1260번.js
+++ coding-test/BaekJoon/1260번.js
@@ -1,94 +1,138 @@
-class Graph {
-    constructor() {
-        this.adjacencyList = {};
-    }
-    addVertex(vertex) {
-        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-    }
-    addEdge(v1, v2) {
-        if (!this.adjacencyList[v1].includes(v2)) this.adjacencyList[v1].push(v2);
-        if (!this.adjacencyList[v2].includes(v1)) this.adjacencyList[v2].push(v1);
-    }
-}
+// class Graph {
+//     constructor() {
+//         this.adjacencyList = {};
+//     }
+//     addVertex(vertex) {
+//         if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+//     }
+//     addEdge(v1, v2) {
+//         if (!this.adjacencyList[v1].includes(v2)) this.adjacencyList[v1].push(v2);
+//         if (!this.adjacencyList[v2].includes(v1)) this.adjacencyList[v2].push(v1);
+//     }
+// }
 
-const graph = new Graph();
-let visited = [];
-
-
-function DFS(start, visited) {
-    visited[start] = true;
-    process.stdout.write(start + ' ');
-
-    for (let i = 0; i < graph.adjacencyList[start].length; i++) {
-        const adjacentVertex = graph.adjacencyList[start][i];
-        if (!visited[adjacentVertex]) {
-            DFS(adjacentVertex, visited);
-        }
-    }
-}
-
-function BFS(start, visited) {
-    let queue = [start];
-    visited[start] = true;
-
-    while (queue.length > 0) {
-        const elem = queue.shift();
-        process.stdout.write(elem + ' ');
-
-        for (let i = 0; i < graph.adjacencyList[elem].length; i++) {
-            const adjacentVertex = graph.adjacencyList[elem][i];
-            if (!visited[adjacentVertex]) {
-                visited[adjacentVertex] = true;
-                queue.push(adjacentVertex);
-            }
-        }
-    }
-}
+// const graph = new Graph();
+// let visited = [];
 
 
+// function DFS(start, visited) {
+//     visited[start] = true;
+//     process.stdout.write(start + ' ');
 
-//공백이 포함된 문자 여러 줄 입력 받기
-const readline = require("readline");
+//     for (let i = 0; i < graph.adjacencyList[start].length; i++) {
+//         const adjacentVertex = graph.adjacencyList[start][i];
+//         if (!visited[adjacentVertex]) {
+//             DFS(adjacentVertex, visited);
+//         }
+//     }
+// }
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+// function BFS(start, visited) {
+//     let queue = [start];
+//     visited[start] = true;
+
+//     while (queue.length > 0) {
+//         const elem = queue.shift();
+//         process.stdout.write(elem + ' ');
+
+//         for (let i = 0; i < graph.adjacencyList[elem].length; i++) {
+//             const adjacentVertex = graph.adjacencyList[elem][i];
+//             if (!visited[adjacentVertex]) {
+//                 visited[adjacentVertex] = true;
+//                 queue.push(adjacentVertex);
+//             }
+//         }
+//     }
+// }
+
+
+
+// //공백이 포함된 문자 여러 줄 입력 받기
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// let input = [];
+
+// rl.on('line', (line) => {
+//     input.push(line.split(' ').map(el => parseInt(el)));
+// })
+
+// rl.on('close', () => {
+//     const N = input[0][0]; //정점 개수
+//     const M = input[0][1]; //간선 개수
+//     const V = input[0][2]; // 탐색 시작 정점 번호
+
+//     for (let i = 1; i <= M; i++) {
+//         const [v1, v2] = input[i];
+//         graph.addVertex(v1);
+//         graph.addVertex(v2);
+//         graph.addEdge(v1, v2);
+//     }
+
+//     for (let key in graph.adjacencyList) {
+//         graph.adjacencyList[key].sort((a, b) => a - b);
+//     }
+
+//     visited = Array(N + 1).fill(false);
+
+//     DFS(V, visited);
+
+//     console.log(); //줄 바꿈
+
+//     visited = Array(N + 1).fill(false);
+
+//     BFS(V, visited);
+
+//     console.log(); //줄 바꿈
+
+//     process.exit();
+// })
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const [N, M, V] = input.shift().split(' ').map(Number);
+const edges = input.map(v => v.split(' ').map(Number));
+const graph = [...Array(N + 1)].map(() => []);
+edges.forEach(([from, to]) => {
+    graph[from].push(to);
+    graph[to].push(from);
 });
 
-let input = [];
-
-rl.on('line', (line) => {
-    input.push(line.split(' ').map(el => parseInt(el)));
-})
-
-rl.on('close', () => {
-    const N = input[0][0]; //정점 개수
-    const M = input[0][1]; //간선 개수
-    const V = input[0][2]; // 탐색 시작 정점 번호
-
-    for (let i = 1; i <= M; i++) {
-        const [v1, v2] = input[i];
-        graph.addVertex(v1);
-        graph.addVertex(v2);
-        graph.addEdge(v1, v2);
+const dfs = (start) => {
+    const stack = [start];
+    const visited = Array(N + 1).fill(false);
+    const order = [];
+    while (stack.length) {
+        const node = stack.pop();
+        if (!visited[node]) {
+            visited[node] = true;
+            order.push(node);
+            stack.push(...graph[node]);
+        }
     }
+    return order.join(' ');
+};
 
-    for (let key in graph.adjacencyList) {
-        graph.adjacencyList[key].sort((a, b) => a - b);
+const bfs = (start) => {
+    const queue = [start];
+    const visited = Array(N + 1).fill(false);
+    const order = [];
+    while (queue.length) {
+        const node = queue.shift();
+        if (!visited[node]) {
+            visited[node] = true;
+            order.push(node);
+            queue.push(...graph[node]);
+        }
     }
+    return order.join(' ');
+};
 
-    visited = Array(N + 1).fill(false);
+graph.forEach(v => v.sort((a, b) => b - a));
+console.log(dfs(V));
 
-    DFS(V, visited);
-
-    console.log(); //줄 바꿈
-
-    visited = Array(N + 1).fill(false);
-
-    BFS(V, visited);
-
-    console.log(); //줄 바꿈
-
-    process.exit();
-})
-
+graph.forEach(v => v.sort((a, b) => a - b));
+console.log(bfs(V));
