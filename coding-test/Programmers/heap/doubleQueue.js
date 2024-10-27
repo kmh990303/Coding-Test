b18adce5d4544dc5b320f7af5a21cb9527a18afa
Minhyuk Kim <labcs@naver.com>
2024-10-27 12:59:39 +0900

    힙 문제 풀이 및 복습

--- coding-test/Programmers/heap/doubleQueue.js
+++ coding-test/Programmers/heap/doubleQueue.js
@@ -13,34 +13,70 @@
 // 원소는 “명령어 데이터” 형식으로 주어집니다.- 최댓값/최솟값을 삭제하는 연산에서 최댓값/최솟값이 둘 이상인 경우, 하나만 삭제합니다.
 // 빈 큐에 데이터를 삭제하라는 연산이 주어질 경우, 해당 연산은 무시합니다.
 
+// function solution(operations) {
+//     let queue = [];
+
+//     for (let i = 0; i < operations.length; i++) {
+//         const[mode, num] = operations[i].split(' ');
+//         let n = Number(num); //실제 숫자로 바꿔서 저장
+
+//         if (queue.length > 0) {
+//             if (mode === 'D') {
+//                 if (n === 1) {
+//                     let maxN = Math.max(...queue);
+//                     let maxIdx = queue.findIndex((val) => val === maxN);
+//                     queue.splice(maxIdx, 1);
+//                 } else {
+//                     let minN = Math.min(...queue);
+//                     let minIdx = queue.findIndex((val) => val === minN);
+//                     queue.splice(minIdx, 1);
+//                 }
+//             }
+//         }
+//         if (mode === 'I') {
+//             queue.push(n);
+//         }
+//     }
+
+//     if (queue.length === 0) {
+//         return [0, 0];
+//     } else {
+//         return [Math.max(...queue), Math.min(...queue)];
+//     }
+// }
+
+
+// 복습  => 힙은 push를 하던 pop을 하던 주어진 규칙에 따라 다시 정렬시켜야 하는 구조
 function solution(operations) {
-    let queue = [];
-    
-    for (let i = 0; i < operations.length; i++) {
-        const[mode, num] = operations[i].split(' ');
-        let n = Number(num); //실제 숫자로 바꿔서 저장
-        
-        if (queue.length > 0) {
-            if (mode === 'D') {
-                if (n === 1) {
-                    let maxN = Math.max(...queue);
-                    let maxIdx = queue.findIndex((val) => val === maxN);
-                    queue.splice(maxIdx, 1);
+    const queue = [];
+
+    function addToHeap(value) {
+        queue.push(value);
+        queue.sort((a, b) => b - a); // 내림 차순 정렬
+    }
+
+    operations.forEach((operation) => {
+        const [command, value] = operation.split(' ');
+
+        if (command === 'I') {
+            addToHeap(+value);
+        } else {
+            if (queue.length > 0) {
+                if (value === '1') {
+                    queue.shift();
                 } else {
-                    let minN = Math.min(...queue);
-                    let minIdx = queue.findIndex((val) => val === minN);
-                    queue.splice(minIdx, 1);
+                    queue.pop();
                 }
             }
         }
-        if (mode === 'I') {
-            queue.push(n);
-        }
-    }
-   
-    if (queue.length === 0) {
-        return [0, 0];
+    })
+
+
+    if (queue.length > 0) {
+        return [queue[0], queue[queue.length - 1]];
     } else {
-        return [Math.max(...queue), Math.min(...queue)];
+        return [0, 0];
     }
 }
+
+// 문자열을 split 하면 모두 문자열 자료형임
