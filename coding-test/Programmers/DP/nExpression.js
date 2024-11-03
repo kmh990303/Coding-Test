4bd6d2176b3ce3bc9e9f2bd4b9b63442e2b5f076
Minhyuk Kim <labcs@naver.com>
2024-11-03 11:18:57 +0900

    DP 문제 풀이 및 복습

--- coding-test/Programmers/DP/nExpression.js
+++ coding-test/Programmers/DP/nExpression.js
@@ -25,3 +25,32 @@ function solution(N, number) { // N을 최대 8번 사용해서 number를 만드
 
     return -1;
 }
+
+
+// 복습
+function solution(N, number) {
+    if (N === number) return 1;
+
+    const dp = Array.from({ length: 9 }, () => new Set());
+
+    for (let i = 1; i <= 8; i++) {
+        dp[i].add(+String(N).repeat(i));
+    }
+
+
+    for (let i = 1; i <= 8; i++) {
+        for (let j = 1; j < i; j++) {
+            for (const num1 of dp[j]) {
+                for (const num2 of dp[i - j]) {
+                    dp[i].add(num1 + num2);
+                    dp[i].add(num1 - num2);
+                    dp[i].add(num1 * num2);
+                    if (num2 !== 0) dp[i].add(num1 / num2);
+                }
+            }
+        }
+        if (dp[i].has(number)) return i;
+    }
+
+    return -1;
+}
