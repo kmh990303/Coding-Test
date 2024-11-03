a35e268191b6c40ddbc64c77add01f4e4cc60795
Minhyuk Kim <labcs@naver.com>
2024-11-03 12:48:02 +0900

    DP 문제 풀이 및 복습

--- coding-test/Programmers/DP/schoolPath.js
+++ coding-test/Programmers/DP/schoolPath.js
@@ -105,3 +105,33 @@ function solution(m, n, puddles) {
     return dp[n - 1][m - 1]; // 학교에 도착하는 경로 수
 }
 
+
+// 복습
+
+function solution(m, n, puddles) {
+    const MOD = 1_000_000_007;
+
+    const dp = Array.from({ length: n }, () => Array(m).fill(0));
+
+    dp[0][0] = 1;
+
+    puddles.forEach(([x, y]) => {
+        dp[y - 1][x - 1] = -1;
+    })
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (dp[i][j] === -1) continue;
+
+            if (i > 0 && dp[i - 1][j] !== -1) {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+            }
+
+            if (j > 0 && dp[i][j - 1] !== -1) {
+                dp[i][j] = (dp[i][j] + dp[i][j - 1]) % MOD;
+            }
+        }
+    }
+
+    return dp[n - 1][m - 1];
+}
