c4331582e9c4a6f34d16f388f2080ed3d3d08860
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-11-16 13:28:30 +0900

    [Gold IV] Title: 전구와 스위치, Time: 80 ms, Memory: 34384 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -2,37 +2,37 @@ import sys
 
 input = sys.stdin.readline
 
-N = int(input())
-
-def reverse(bulbs, count):
+def change(bulbs, count):
     for i in range(1, N - 1):
         if bulbs[i - 1] != target[i - 1]:
             count += 1
-            for j in range(i-1, i+2):
+            for j in range(i - 1, i + 2):
                 bulbs[j] = not bulbs[j]
     if bulbs[N - 1] != target[N - 1]:
         count += 1
-        bulbs[N - 2] = not bulbs[N - 2]
-        bulbs[N - 1] = not bulbs[N - 1]
+        for j in range(N - 2, N):
+            bulbs[j] = not bulbs[j]
+
     if bulbs == target:
         return count
     else:
         return -1
 
-off = list(map(bool, map(int, input().rstrip())))
+N = int(input())
 
+off = list(map(bool, list(map(int, input().rstrip()))))
 on = off.copy()
 on[0] = not on[0]
 on[1] = not on[1]
 
-target = list(map(bool, map(int, input().rstrip())))
+target = list(map(bool, list(map(int, input().rstrip()))))
 
 if off == target:
     print(0)
 else:
-    count = reverse(off, 0)
+    count = change(off, 0)
     if count != -1:
         print(count)
     else:
-        count = reverse(on, 1)
+        count = change(on, 1)
         print(count if count else -1)
