a64de2880578bf8de83e18e9b07f53a8e04033c5
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-11-24 16:14:06 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 72 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,38 +1,43 @@
-from collections import deque
+import sys
+from collections import  deque
+input = sys.stdin.readline
 
 def can_reach(start, stores, end):
     points = [start] + stores + [end]
     n = len(points)
     graph = [[] for _ in range(n)]
 
-    # 그래프 생성 : 두 좌표 사이 거리가 1000 이하인 경우 간선 연결
+    # 인접 정점 연결
     for i in range(n):
         for j in range(n):
             if i != j:
                 x1, y1 = points[i]
                 x2, y2 = points[j]
-                if abs(x1 - x2) + abs(y1 - y2) <= 1000:
-                    graph[i].append(j) # 연결 가능한 정점의 인덱스 저장
+                if abs(x1-x2) + abs(y1-y2) <= 1000:
+                    graph[i].append(j)
+    # BFS 탐색
     visited = [False] * n
-    q = deque([0])
+    queue = deque([0])
     visited[0] = True
 
-    while q:
-        current = q.popleft()
+    while queue:
+        current = queue.popleft()
+
         if current == n - 1:
             return 'happy'
 
         for neighbor in graph[current]:
             if not visited[neighbor]:
                 visited[neighbor] = True
-                q.append(neighbor)
-    return "sad"
+                queue.append(neighbor)
+    return 'sad'
 
 t = int(input())
+
 for _ in range(t):
     n = int(input())
     start = tuple(map(int, input().split()))
     stores = [tuple(map(int, input().split())) for _ in range(n)]
     end = tuple(map(int, input().split()))
-
+    
     print(can_reach(start, stores, end))
