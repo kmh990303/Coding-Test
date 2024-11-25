186d989174d9276ecb6bb9d7ba5749e2fef4e36c
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-11-25 12:51:07 +0900

    [Gold V] Title: A와 B, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -2,17 +2,18 @@ import sys
 
 input = sys.stdin.readline
 
-def check_transform(T, S):
-    while len(T) > len(S):
-        if T[-1] == 'A':  # 맨 끝이 'A'인 경우 A를 제거
-            T = T[:-1]
-        elif T[-1] == 'B':  # 맨 끝이 'B'인 경우 B를 제거 후 뒤집기
-            T = T[:-1][::-1]
-
-    # 변환 완료 후 결과 비교
-    return 1 if T == S else 0
+def check_transform(s, t):
+    while (len(t) > len(s)):
+        if t[-1] == 'A':
+            t = t[:-1]
+        elif t[-1] == 'B':
+            t = t[:-1][::-1]
+    if t == s:
+        print(1)
+    else:
+        print(0)
 
 S = input().strip()
 T = input().strip()
 
-print(check_transform(T, S))
+check_transform(S, T)
