3d49bdbe7b32828c20bf7380a6b37d00a2c5bad1
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-11-27 22:08:19 +0900

    [Gold III] Title: 괄호 추가하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.py
@@ -4,13 +4,14 @@ st = input().strip()
 numbers = []
 operators = []
 
+max_result = -pow(2, 31)
+
 for ch in st:
     if '0' <= ch <= '9':
         numbers.append(int(ch))
     else:
         operators.append(ch)
 
-max_result = -pow(2, 31)
 
 def calculate(a, op, b):
     if op == '+':
@@ -20,23 +21,21 @@ def calculate(a, op, b):
     elif op == '*':
         return a * b
 
-# 조합 탐색은 dfs나 bfs 활용할 것
 def dfs(idx, current_result):
     global max_result
 
-    # 마지막 숫자까지 탐색한 경우
     if idx == len(operators):
         max_result = max(max_result, current_result)
         return
 
-    # 괄호를 적용하지 않은 경우
+    # 괄호를 사용하지 않는 경우
     next_result = calculate(current_result, operators[idx], numbers[idx + 1])
     dfs(idx + 1, next_result)
 
-    # 괄호를 적용한 경우
+    # 괄호를 사용하는 경우
     if idx + 1 < len(operators):
-        temp = calculate(numbers[idx + 1], operators[idx + 1], numbers[idx + 2])
-        next_result = calculate(current_result, operators[idx], temp)
+        tmp = calculate(numbers[idx + 1], operators[idx + 1], numbers[idx + 2])
+        next_result = calculate(current_result, operators[idx], tmp)
         dfs(idx + 2, next_result)
 
 dfs(0, numbers[0])
