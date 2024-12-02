61f71b2bee9fd8cbed27d38bf20b0bdaa5afff35
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-12-02 15:27:58 +0900

    [Gold III] Title: 불!, Time: 2000 ms, Memory: 71592 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -10,35 +10,36 @@ jihun_queue = deque()
 fire_time = [[-1] * c for _ in range(r)]
 jihun_time = [[-1] * c for _ in range(r)]
 
-# 초기 상태 설정
+direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
 for i in range(r):
     for j in range(c):
+        if board[i][j] == 'F':
+            fire_queue.append((i, j))
+            fire_time[i][j] = 0
         if board[i][j] == 'J':
             jihun_queue.append((i, j))
             jihun_time[i][j] = 0
-        elif board[i][j] == 'F':
-            fire_queue.append((i, j))
-            fire_time[i][j] = 0
-
-direction = [(-1, 0), (0, 1), (1, 0), (0, -1)] # 북 동 남 서
 
 while fire_queue:
     x, y = fire_queue.popleft()
+
     for dx, dy in direction:
         nx, ny = x + dx, y + dy
         if 0 <= nx < r and 0 <= ny < c and fire_time[nx][ny] == -1 and board[nx][ny] != '#':
-            fire_time[nx][ny] = fire_time[x][y] + 1
             fire_queue.append((nx, ny))
+            fire_time[nx][ny] = fire_time[x][y] + 1
 
 while jihun_queue:
     x, y = jihun_queue.popleft()
+
     for dx, dy in direction:
         nx, ny = x + dx, y + dy
         if 0 <= nx < r and 0 <= ny < c:
             if jihun_time[nx][ny] == -1 and board[nx][ny] != '#':
                 if fire_time[nx][ny] == -1 or jihun_time[x][y] + 1 < fire_time[nx][ny]:
-                    jihun_time[nx][ny] = jihun_time[x][y] + 1
                     jihun_queue.append((nx, ny))
+                    jihun_time[nx][ny] = jihun_time[x][y] + 1
         else:
             print(jihun_time[x][y] + 1)
             exit()
