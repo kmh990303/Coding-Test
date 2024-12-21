7f52967a47cd449a235a833be330f98108797b46
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-12-21 21:13:30 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 220 ms, Memory: 24400 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
@@ -2,46 +2,44 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-});
+})
 
 const input = [];
 
 rl.on('line', (line) => {
     input.push(line);
 }).on('close', () => {
-    const n = Number(input[0]); // 길의 길이
-    const m = Number(input[1]); // 가로등의 개수
+    const n = Number(input[0]);
+    const m = Number(input[1]);
     const locations = input[2].split(' ').map(Number);
 
-    let left = 1; // 최소 높이
-    let right = n; // 최대 높이
+    let left = 1;
+    let right = n;
     let answer = n;
 
-    const canCover = (height) => {
-        let currentEnd = 0; // 현재 밝힌 범위의 끝
+    function canCover(height) {
+        let currentEnd = 0;
 
         for (const location of locations) {
             if (location - height > currentEnd) {
-                // 현재 밝히지 못한 구간이 있다면 높이가 부족
                 return false;
             }
-            currentEnd = location + height; // 현재 가로등으로 커버되는 범위
+            currentEnd = location + height;
         }
 
-        return currentEnd >= n; // 끝까지 밝히는지 확인
-    };
+        return currentEnd >= n;
+    }
 
-    // 이진 탐색
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
 
         if (canCover(mid)) {
-            answer = mid; // 커버 가능하면 높이를 줄여본다
+            answer = mid;
             right = mid - 1;
         } else {
-            left = mid + 1; // 커버 불가능하면 높이를 늘린다
+            left = mid + 1;
         }
     }
 
     console.log(answer);
-});
+})
