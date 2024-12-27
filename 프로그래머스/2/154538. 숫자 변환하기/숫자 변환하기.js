9930c7feedbbb71760adfecbfaad67d8d51ab7df
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-12-27 11:05:52 +0900

    [level 2] Title: 숫자 변환하기, Time: 1509.16 ms, Memory: 263 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,10 +1,13 @@
 function solution(x, y, n) {
-    const dp = new Array(y+1).fill(Infinity);
+    const queue = [[x, 0]];
+    const dp = Array(y + 1).fill(Infinity);
     dp[x] = 0;
-    for(let i=x; i<=y; i++){
-        dp[i+n] = Math.min(dp[i+n],dp[i]+1);
-        dp[i*2] = Math.min(dp[i*2],dp[i]+1);
-        dp[i*3] = Math.min(dp[i*3],dp[i]+1);
+    
+    for (let i = x; i <= y; i++) {
+        dp[i + n] = Math.min(dp[i + n], dp[i] + 1);
+        dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
+        dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
     }
-    return dp[y]!==Infinity? dp[y] : -1;
+    
+    return dp[y] === Infinity ? -1 : dp[y];
 }
