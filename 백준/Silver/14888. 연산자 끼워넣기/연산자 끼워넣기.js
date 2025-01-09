6a1bcdc286815358cc1c28e48a9c461bb89d361f
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-01-09 09:40:32 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 184 ms, Memory: 11060 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -1,40 +1,43 @@
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .split("\n")
-  .map((v) => v.split(" ").map(Number));
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-const [N, nums, operator] = input;
+const input = [];
 
-let max = Number.MIN_SAFE_INTEGER;
-let min = Number.MAX_SAFE_INTEGER;
+rl.on('line', (line) => {
+    input.push(line);
+}).on('close', () => {
+    const n = Number(input[0]);
+    const arr = input[1].split(' ').map(Number);
+    const calArr = input[2].split(' ').map(Number);
+    let maxVal = Number.MIN_SAFE_INTEGER
+    let minVal = Number.MAX_SAFE_INTEGER
 
-const calculator = [
-  (a, b) => a + b,
-  (a, b) => a - b,
-  (a, b) => a * b,
-  (a, b) => parseInt(a / b),
-];
+    const calculator = [
+        (a, b) => a + b,
+        (a, b) => a - b,
+        (a, b) => a * b,
+        (a, b) => parseInt(a / b),
+    ];
 
-const dfs = (count, result) => {
-  //연산자 갯수만큼 반복하다가 max, min 판정
-  if (count === N - 1) {
-    max = Math.max(max, result);
-    min = Math.min(min, result);
-  } else {
-    for (let i = 0; i < operator.length; i++) {
-      if (operator[i] === 0) {
-        continue;
-      }
-      operator[i]--;
-      dfs(count + 1, calculator[i](result, nums[count + 1]));
-      //배열 원상복구
-      operator[i]++;
+    const dfs = (count, result) => {
+        if (count === n - 1) {
+            maxVal = Math.max(maxVal, result);
+            minVal = Math.min(minVal, result);
+        } else {
+            for (let i = 0; i < calArr.length; i++) {
+                if (calArr[i] === 0) continue;
+                calArr[i]--;
+                dfs(count + 1, calculator[i](result, arr[count + 1]));
+                calArr[i]++;
+            }
+        }
     }
-  }
-};
 
-dfs(0, nums[0]);
+    dfs(0, arr[0]);
 
-console.log(max ? max : 0);
-console.log(min ? min : 0);
+    console.log(maxVal ? maxVal : 0);
+    console.log(minVal ? minVal : 0);
+})
