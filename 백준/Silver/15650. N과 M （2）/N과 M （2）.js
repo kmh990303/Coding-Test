7fd85cddb82535d5e37b7eeb6d527d240664cbf9
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-01-09 10:45:49 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 9484 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+    input.push(line);
+}).on('close', () => {
+    const [n, m] = input[0].split(' ').map(Number);
+    const result = [];
+    const path = [];
+
+    function dfs(depth, start) {
+        if (depth === m) {
+            result.push(path.join(' '));
+            return;
+        }
+
+        for (let i = start; i <= n; i++) {
+            path.push(i);
+            dfs(depth + 1, i + 1);
+            path.pop();
+        }
+    }
+
+    dfs(0, 1);
+    console.log(result.join('\n'));
+})
