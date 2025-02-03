1e91103f878f8c95d77141a859962b2e2c4a41cd
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-02-03 21:05:57 +0900

    [Silver II] Title: 촌수계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,12 +1,10 @@
-def dfs(start, target, dist): # start가 시작점, target이 목표 지점
-    global relation
-    global visited
-
+def dfs(start, target, dist):
+    global visited, relation
     if start == target:
         return dist
-
-    visited[start] = True
+    
     result = -1
+    visited[start] = True
 
     for i in range(1, n + 1):
         if relation[start][i] == 1 and not visited[i]:
@@ -15,24 +13,22 @@ def dfs(start, target, dist): # start가 시작점, target이 목표 지점
                 result = temp
                 break
 
-    return result
-
+    return result 
 
 
 n = int(input())
 
 t1, t2 = map(int, input().split())
 
-relation = [[0] * (n + 1) for _ in range(n + 1)] # 부모 자식의 관계를 저장하는 배열
-
 t = int(input())
+
+relation = [[0] * (n + 1) for _ in range(n + 1)]
 visited = [False] * (n + 1)
 
 for _ in range(t):
     parent, child = map(int, input().split())
     relation[parent][child] = 1
     relation[child][parent] = 1
-
-realResult = dfs(t1, t2, 0)
-
-print(realResult)
+    
+answer = dfs(t1, t2, 0)
+print(answer)
