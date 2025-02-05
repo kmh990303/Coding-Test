a66ad5f8e3cab669c13d3ef2c64cc600e87423f0
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-02-05 22:39:44 +0900

    [Gold V] Title: 강의실 배정, Time: 372 ms, Memory: 75768 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,21 @@
+import sys, heapq
+input = sys.stdin.readline 
+
+t = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(t)]
+
+arr.sort()
+
+heap = []
+
+heapq.heappush(heap, arr[0][1])
+
+for i in range(1, t):
+    if arr[i][0] < heap[0]:
+        heapq.heappush(heap, arr[i][1])
+    else:
+        heapq.heappop(heap)
+        heapq.heappush(heap, arr[i][1])
+
+print(len(heap))
