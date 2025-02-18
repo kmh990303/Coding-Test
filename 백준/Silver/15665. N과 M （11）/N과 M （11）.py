c9ebc213cc38c318ff50490dcecd81438ddbd4f0
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-02-18 12:46:50 +0900

    [Silver II] Title: N과 M (11), Time: 912 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+
+result = [0] * m
+
+def back_tracking(depth, index):
+    if depth == m:
+        for elem in result:
+            print(elem, end = ' ')
+        print()
+
+        return
+    tmp = 0
+    for i in range(index, n):
+        if tmp != arr[i]:
+            result[depth] = arr[i]
+            tmp = arr[i]
+            back_tracking(depth + 1, 0)
+
+back_tracking(0, 0)
