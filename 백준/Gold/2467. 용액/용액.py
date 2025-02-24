e448d16c5fe537aa107e9a58af346af43c9d1dd8
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-02-25 08:28:54 +0900

    [Gold V] Title: 용액, Time: 108 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,26 +1,26 @@
 import sys
 
-input = sys.stdin.readline 
+input = sys.stdin.readline
 
 n = int(input())
-arr = list(map(int,input().split()))
+arr = list(map(int, input().split()))
 
 left = 0
 right = n - 1
 
 st = abs(arr[left] + arr[right])
-value1, value2 = arr[left], arr[right]
+value1 = arr[left]
+value2 = arr[right]
 
 while left < right:
     mix = arr[left] + arr[right]
-    
     if abs(mix) < st:
         st = abs(mix)
-        value1, value2 = arr[left], arr[right]
-    
-    if mix < 0: 
-        left += 1
-    else:
+        value1,value2 = arr[left], arr[right]
+
+    if mix > 0:
         right -= 1
+    else:
+        left += 1
 
 print(value1, value2)
