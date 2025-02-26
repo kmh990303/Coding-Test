ea18fb5a4964f68c56024b315c0fab69c364f6fc
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-02-26 17:01:07 +0900

    [Gold V] Title: LCS, Time: 212 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,18 +1,18 @@
-def lcs(a, b):
-    n = len(a)
-    m = len(b)
-
+def lcs(st1, st2):
+    n = len(st1)
+    m = len(st2)
     dp = [[0] * (m + 1) for _ in range(n + 1)]
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
-            if a[i - 1] == b[j - 1]:
-                dp[i][j] = dp[i- 1][j - 1] + 1
+            if st1[i - 1] == st2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
     return dp[n][m]
 
-a = input().strip()
-b = input().strip()
+st1 = input().strip()
+st2 = input().strip()
 
-print(lcs(a, b))
+print(lcs(st1, st2))
