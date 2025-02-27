e29979cacba5849d117a5b34441f2c68f5e9c517
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-02-27 15:43:34 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 384 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -2,14 +2,13 @@ import sys
 
 input = sys.stdin.readline
 
-
 def getWayNum(target):
-    dp = [1] * 10001
+    dp = [1] * (target + 1) # 1로만 만드는 경우의 수를 초기값으로 설정 // 모든 값 모두 1로만 만들 수 있음
 
-    for i in range(2, 10001):
+    for i in range(2, target + 1):
         dp[i] += dp[i - 2]
 
-    for i in range(3, 10001):
+    for i in range(3, target + 1):
         dp[i] += dp[i - 3]
 
     return dp[target]
