94c70c76da78df293199e788244e0559cc917508
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-03-14 12:41:41 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -1,30 +1,30 @@
+# 1700번 멀티탭 스케줄링
+
 n, k = map(int, input().split())
+use = list(map(int, input().split()))
 
-uses = list(map(int, input().split()))
-
-plug = []
 answer = 0
+plug = []
 
-for i in range(k):
-    if uses[i] in plug:
+for idx in range(k):
+    if use[idx] in plug:
         continue
-
+        
     if len(plug) != n:
-        plug.append(uses[i])
+        plug.append(use[idx])
         continue
-
+    
     priority = []
-
-    # 플러그의 자리가 꽉 차있으면서 새로운 제품의 코드를 꽂아야 하는 경우
-    for c in plug:
-        if c in uses[i:]:
-            priority.append(uses[i:].index(c))
+    
+    for p in plug:
+        if p in use[idx:]:
+            priority.append(use[idx:].index(p))
         else:
             priority.append(101)
-
+    
     target = priority.index(max(priority))
     plug.remove(plug[target])
-    plug.append(uses[i])
+    plug.append(use[idx])
     answer += 1
 
 print(answer)
