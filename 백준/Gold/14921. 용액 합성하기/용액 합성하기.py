2418591cf69a2fa5b8f565b571461c00a915f50b
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-03-29 15:18:32 +0900

    [Gold V] Title: 용액 합성하기, Time: 92 ms, Memory: 43724 KB -BaekjoonHub

--- 백준/Gold/14921. 용액 합성하기/용액 합성하기.py
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.py
@@ -5,19 +5,16 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int, input().split()))
 
-left = 0
-right = n - 1
+left, right = 0, n - 1
 answer = arr[left] + arr[right]
 
 while left < right:
     sum_val = arr[left] + arr[right]
     if abs(answer) > abs(sum_val):
         answer = sum_val
-    
-    # 합이 0보다 크면 right을 줄임
+
     if sum_val > 0:
         right -= 1
-    # 합이 0보다 작으면 left 증가
     else:
         left += 1
 
