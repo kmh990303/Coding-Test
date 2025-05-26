01743b4c937516321e62274ad26230619ec99482
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2025-05-26 19:38:38 +0900

    [Gold IV] Title: 빙산, Time: 2420 ms, Memory: 35952 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,20 +1,24 @@
+# 이전 상태를 저장해놓고 그거 기준으로 인접해있는 0의 개수만큼 빙산 높이 차감
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
+
 board = [list(map(int, input().split())) for _ in range(n)]
 
+# 최초 빙산 위치 지정
+ice_pos = [(i, j) for i in range(n) for j in range(m) if board[i][j] > 0]
+
+# 북동남서
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
-
-# 최초 빙산 위치 저장
-ice_pos = [(i, j) for i in range(n) for j in range(m) if board[i][j] > 0]
 
 def change_ice():
     new_board = [row[:] for row in board]
     new_ice_pos = []
+
     for i, j in ice_pos:
         cnt = 0
         for k in range(4):
@@ -26,35 +30,43 @@ def change_ice():
             new_ice_pos.append((i, j))
     return new_board, new_ice_pos
 
+
+
 def bfs(start, visited):
-    q = deque([start])
-    visited[start[0]][start[1]] = True
-    while q:
-        x, y = q.popleft()
+    # 빙산 덩어리 개수 파악
+    start_x, start_y = start
+    visited[start_x][start_y] = True
+    queue = deque([(start_x, start_y)])
+
+    while queue:
+        cur_i, cur_j = queue.popleft()
+
         for k in range(4):
-            ni, nj = x + dx[k], y + dy[k]
+            ni, nj = cur_i + dx[k], cur_j + dy[k]
             if 0 <= ni < n and 0 <= nj < m:
                 if not visited[ni][nj] and board[ni][nj] > 0:
                     visited[ni][nj] = True
-                    q.append((ni, nj))
+                    queue.append((ni, nj))
+
 
 time = 0
 
 while True:
+    split_cnt = 0
     visited = [[False] * m for _ in range(n)]
-    split = 0
 
     for x, y in ice_pos:
         if not visited[x][y] and board[x][y] > 0:
             bfs((x, y), visited)
-            split += 1
-        if split >= 2:
+            split_cnt += 1
+        if split_cnt >= 2:
             break
 
-    if split >= 2:
+    if split_cnt >= 2:
         print(time)
         break
-    if not ice_pos:  # 빙산이 다 녹았을 경우
+    
+    if not ice_pos:
         print(0)
         break
 
